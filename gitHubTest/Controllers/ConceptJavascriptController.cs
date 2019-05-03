8a46c302ff134a4b1e93a4c1a47d4996978f9fd5
minsong999 <minsong998@gmail.com>
2019-05-03 22:58:38 +0900

    Javascript CallStack Commit

---
+++ gitHubTest/Controllers/ConceptJavascriptController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using gitHubTest.Models;
+
+
+namespace gitHubTest.Controllers
+{
+    public class ConceptJavascriptController : Controller
+    {
+        public IActionResult CallStack()
+        {
+            return View();
+        }
+    }
+}
