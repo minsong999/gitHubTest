6dbdcf17ffb9bd52d0cd834ab5fb93e1b47ac765
minsong999 <minsong998@gmail.com>
2019-05-11 23:07:56 +0900

    cSharp 2. dataType

--- gitHubTest/Controllers/CSharpController.cs
+++ gitHubTest/Controllers/CSharpController.cs
@@ -20,5 +20,17 @@ namespace gitHubTest.Controllers
             Console.WriteLine("Hello C#"); //보기 - 출력 - 출력보기 선택에서 ASP.NET Core 웹 서버을 선택하면 확인할 수 있다.
             return View();
         }
+
+        public IActionResult dataType()
+        {
+            //윈폼을 설명하는데 잘 안 쓰니까... -> ToolBox로 끌어다가 디자인적 요소를 쓸 수 있고 소스보기로 소스를 작성할 수 있다.
+            int num1 = 10;
+            string operator1 = "+";
+            bool isCorrect = true;
+
+            
+            return View();
+        }
+        
     }
 }
