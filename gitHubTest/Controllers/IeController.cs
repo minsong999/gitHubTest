fc822ac584bb21e7017c87f9b1fd69745a190d0a
minsong999 <minsong998@gmail.com>
2019-05-20 21:21:34 +0900

    1. IE 이슈 protocol-relative URL 이란?

---
+++ gitHubTest/Controllers/IeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using gitHubTest.Models;
+
+namespace gitHubTest.Controllers
+{
+    public class IeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult protocolRelativeURL()
+        {
+            return View();
+        }
+
+    }
+}
