7b033a513e2e9d6cd28e702f60a3411119c01b03
minsong999 <minsong998@gmail.com>
2019-05-27 23:32:35 +0900

    개발토끼 1. ASP.NET 소개 .Net Framework .net Core commit

---
+++ gitHubTest/Controllers/DevelopmentRabbitController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace gitHubTest.Controllers
+{
+    public class DevelopmentRabbitController : Controller
+    {
+        
+        public IActionResult Contents()
+        {
+            return View();
+        }
+
+        public IActionResult introudceASPNET()
+        {
+            return View();
+        }
+
+    }
+}
