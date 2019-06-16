18fc13c0a38d262d8fc8e7fd19aba4efe3a0bdfc
minsong999 <minsong998@gmail.com>
2019-06-16 20:44:19 +0900

    개발토끼 10. LoginForm 만들기

--- gitHubTest/Controllers/AccountController.cs
+++ gitHubTest/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
-﻿using gitHubTest.DataContext;
+﻿using System.Linq;
+using gitHubTest.DataContext;
 using gitHubTest.Models;
+using gitHubTest.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gitHubTest.Controllers
@@ -15,6 +17,42 @@ namespace gitHubTest.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model) //파라미터를 받을 때 마이크로소프트 권장은 model로 받는다.
+        {
+            // ID, 비밀번호 - 필수 입력 값
+            if (ModelState.IsValid)
+            {
+                using (var db = new AspnetNoteDBContext())
+                {
+                    // Linq 표현 쿼리식 - 메서드 체이닝
+                    // => : Go to
+                    // u : 람다식 익명 인수 
+                    //var user = db.Users.
+                    //    FirstOrDefault(u=>u.UserId == model.UserId && u.UserPassword == model.UserPassword); //메모리 누수 발생함
+                    var user = db.Users.
+                        FirstOrDefault(u => u.UserId.Equals(model.UserId) &&
+                        u.UserPassword.Equals(model.UserPassword)); //첫번째 혹은 기본값을 출력함
+
+                    if (user != null)
+                    {
+                        //로그인에 성공했을 때
+                        return RedirectToAction("LoignSuccess", "Account"); //로그인에 성공 페이지로 이동
+                    }
+                }
+                //로그인에 실패했을 때
+                //사용자 ID 자체가 회원가입이 안 된 경우
+                ModelState.AddModelError(string.Empty, "사용자 정보가 올바르지 않습니다.");
+            }
+            return View(model);
+        }
+
+        public IActionResult LoignSuccess()
+        {
+            return View();
+        }
+
 
         /// <summary>
         /// 회원 가입
---
+++ gitHubTest/ViewModel/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gitHubTest.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "사용자 ID를 입력하세요.")]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "사용자 비밀번호를 입력하세요")]
+        public string UserPassword { get; set; }
+
+    }
+}
