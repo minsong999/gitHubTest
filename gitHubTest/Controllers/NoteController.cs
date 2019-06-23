21f1bb5f98be9c5752708ace6565b6ae26f81a9b
minsong999 <minsong998@gmail.com>
2019-06-23 22:25:03 +0900

    개발토끼 12. 게시판 리스트 출력하기

---
+++ gitHubTest/Controllers/NoteController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gitHubTest.DataContext;
+using gitHubTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gitHubTest.Controllers
+{
+    public class NoteController : Controller
+    {
+
+        /// <summary>
+        /// 게시판 리스트
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            using(var db = new AspnetNoteDBContext())
+            {
+                var list = db.Notes.ToList(); //테이블 안의 모든 값을 가져올때 사용
+                return View(list);
+            }
+        }
+
+        /// <summary>
+        /// 게시물 추가
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 수정
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
